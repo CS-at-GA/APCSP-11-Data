4e3138b1046dd9fb8a0b992c93b702be28cf5f85
Mr. Oswald <62300716+gajoswald@users.noreply.github.com>
2022-11-04 18:32:51 +0000

    updated readme and added code.

--- README.md
+++ README.md
@@ -2,12 +2,32 @@
 
 ## Pre-reads
 
+* **Required**[^1]: This _Harvard Business Review_ article [Visualizations That Really Work](https://hbr.org/2016/06/visualizations-that-really-work) is a good overview. If you read articles from HBR frequently, you'll likely run into a paywall. You may be able to use a different browser or private browsing to get around it.
+* **Required**, but skimmable: This _Harvard Business School_ overview of different [data visualization techniques](https://online.hbs.edu/blog/post/data-visualization-techniques)
+* At least one of these three more technical overviews:
+  * [What is Data Visualization](https://www.tableau.com/learn/articles/data-visualization) (Tableu)
+  * [Data Visualization](https://www.ibm.com/cloud/learn/data-visualization#toc-what-is-da-2LvBeFaU) (IBM)
+  * [What is Data Visualization and Why is it Important?](https://www.geeksforgeeks.org/what-is-data-visualization-and-why-is-it-important/) (GeeksforGeeks)
+* Then just take a look at some data visualizations. You could google, or use any of the links below
+  * https://visme.co/blog/best-data-visualizations/
+  * https://www.tableau.com/learn/articles/best-beautiful-data-visualization-examples
+  * https://vega.github.io/vega/
+
+
 ## Starter Code Overview
 
 ## Assignment 
 
+You are to find a dataset and create a way to display the information in the data set in various and potentially enlightening ways. 
+
 ### Requirements
+
+* your data set must have many entries that have multiple objective properties[^2].
+* your data set should be in CSV or JSON format
+* there needs to be a way to change the views of the data. 
 
 ### _Some_ Ideas for Ways to Expand on This Project. 
 
 <!--- Footnotes Below --->
+[^1]: If you can... 
+[^2]: the example data set here has 116 entries (rows/lines), each with 10 properties (columns). I'd call it slightly small, but still rich. 
--- script.js
+++ script.js
@@ -1,12 +1,111 @@
 const colorList = ['maroon','brown','olive','teal','navy','black','red','orange','yellow','lime','green','cyan','blue','purple','magenta','grey','pink','apricot','beige','mint','lavender','white'];
 
+let table;
+let xColumn;
+let selectingX = false;
+let textBoxWidth;
+
+function preload() {
+  table = loadTable("dataR2.csv","csv","header")
+  //https://archive-beta.ics.uci.edu/dataset/451/breast+cancer+coimbra
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   stroke(random(colorList));
   fill(random(colorList));  
+  yColumn = table.columns[0];
+  textBoxWidth = textWidth(`y-axis: ${table.columns.reduce( (longest,current) => textWidth(current) > textWidth(longest) ? current : longest, "")}`)
+  xColumn;
   background(255);
+  noLoop();
+  noStroke();
 }
 
 function draw() {
-  ellipse(mouseX, mouseY, 25, 25);
+  background('white');
+  drawInputPrompt();
+  drawData(xColumn,yColumn,{x:{min:textBoxWidth+20, max:textBoxWidth+20+400}, y:{min:310,max:10}});
 }
+
+function drawData(
+  xAxis = Array.from(Array(table.getRowCount()), (x,i) => (i) ), 
+  yAxis = yColumn, 
+  boundaries = { x: {min: textBoxWidth, max: width-20}, y:{min:height-20,max:20}} ) {
+  noFill();
+  stroke('black');
+  rectMode(CORNERS)
+  rect(boundaries.x.min, boundaries.y.min, boundaries.x.max, boundaries.y.max)
+  noStroke();
+  const classification = table.getColumn("Classification");
+
+  let data = Array.isArray(xAxis) ? xAxis : table.getColumn(xAxis).map( float )
+  const x = {
+    data, 
+    stats: descriptiveStatisticsFor( data )
+  }
+  data = table.getColumn(yAxis).map( float )
+  const y = {
+    data,
+    stats: descriptiveStatisticsFor( data )
+  }
+
+  for( let i = 0; i < classification.length; i++ ) {
+    classification[i] === "1" ? fill(colorList[7]) : fill(colorList[12])
+    const [xMin,xMax] = Array.isArray(xAxis) ? [0,xAxis.length-1] : [x.stats.min, x.stats.max]
+    circle(
+      map( x.data[i], xMin, xMax, boundaries.x.min, boundaries.x.max ),
+      map( y.data[i], y.stats.min, y.stats.max, boundaries.y.min, boundaries.y.max), 3);
+  }
+}
+
+function descriptiveStatisticsFor(data) {
+  const stats = {
+    min: Infinity,
+    max: -Infinity,
+    mean: 0
+  }
+  let sum = 0;
+  for( let x of data ) {
+    x = float(x);
+    sum += x;
+    stats.min = min(x,stats.min);
+    stats.max = max(x, stats.max);
+  }
+  stats.mean = sum/data.length;
+  return stats;
+}
+
+function drawInputPrompt() {
+  fill('black')
+  for( let i = 0; i < table.columns.length-1; i++ ) {
+    text( `${i+1}: ${table.columns[i]}`, 10, 10 + 14 * i )
+  }
+  let y = 10 + 14 * table.columns.length;
+  text( `x axis: ${xColumn ?? "sequence"}`,10,y);
+  text( `y axis: ${yColumn}`, 10, y + 14 );
+  fill(colorList[7]);
+  let x = 10;
+  text( `present`, x, y + 28 );
+  fill(colorList[12]);
+  text( `absent`, x + 5 + textWidth('present'), y + 28 );
+}
+
+function keyPressed() {
+  if( key === 'x' ) {
+    selectingX = true;
+  } else {
+    const digit = parseInt(key)
+    if( digit && digit > 0 && digit < 10 ) {
+      if( selectingX ) {
+        xColumn = table.columns[digit-1];
+        selectingX = false;
+      } else {
+        yColumn = table.columns[digit-1];
+      }
+      redraw();
+    }    
+  }
+
+}
+
